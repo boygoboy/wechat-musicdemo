0d8ed2bcb0b8b532c568843daf11ad0892324b1b
caiwenjie@anosi.cn <caiwenjie@anosi.cn>
2022-05-25 08:58:08 +0800

    云音乐小程序

---
+++ components/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+// components/NavBar/NavBar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:'default title'
+    },
+    recommendtitle:{
+      type:String,
+      value:'value'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
